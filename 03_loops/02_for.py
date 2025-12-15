c01631a2314037eb55bb462e2d984a9765b7c9cf
jordi7aqc <quezadajordan64@gmail.com>
2025-12-15 14:19:23 +0100

    ejercicios resuelto de for

--- 03_loops/02_for.py
+++ 03_loops/02_for.py
@@ -6,17 +6,34 @@
 # Imprime todos los números pares del 2 al 20 (inclusive) usando un bucle for.
 print("\nEjercicio 1:")
 
+for pares in range(2, 21, 2):
+    print(pares)
+
 # Ejercicio 2: Calcular la media de una lista
 # Dada la siguiente lista de números:
 # numeros = [10, 20, 30, 40, 50]
 # Calcula la media de los números usando un bucle for.
 print("\nEjercicio 2:")
 
+numeros = [10, 20, 30, 40, 50]
+suma = 0
+for num in numeros:
+    suma += num
+media = suma / len(numeros)
+print(f"La media es: {media}")
+
 # Ejercicio 3: Buscar el máximo de una lista
 # Dada la siguiente lista de números:
 # numeros = [15, 5, 25, 10, 20]
 # Encuentra el número máximo en la lista usando un bucle for.
 print("\nEjercicio 3:")
+
+numeros = [15, 5, 25, 10, 20]
+maximo = numeros[0]
+for num in numeros:
+    if num > maximo:
+        maximo = num
+print(f"El numero maxximo es: {maximo}")
 
 # Ejercicio 4: Filtrar cadenas por longitud
 # Dada la siguiente lista de palabras:
@@ -25,9 +42,21 @@ print("\nEjercicio 3:")
 # usando un bucle for y list comprehension.
 print("\nEjercicio 4:")
 
+palabras = ["casa", "arbol", "sol", "elefante", "luna"]
+palabras_largas = [palabra for palabra in palabras if len(palabra) > 5]
+print(palabras_largas)
+
 # Ejercicio 5: Contar palabras que empiezan con una letra
 # Dada la siguiente lista de palabras:
 # palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
 # Pide al usuario que introduzca una letra.
 # Cuenta cuántas palabras en la lista empiezan con esa letra (sin diferenciar mayúsculas/minúsculas).
 print("\nEjercicio 5:")
+
+palabras = ["casa", "arbol", "sol", "elefante", "luna", "coche"]
+letra = input("Introduce una letra: ").lower()
+contador = 0
+for palabra in palabras:
+    if palabra.lower().startswith(letra):
+        contador += 1
+print(f"Hay {contador} palabras que empiezan con la letra {letra}")
